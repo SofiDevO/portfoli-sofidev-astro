5880a75b2f6ab9a85e6338096fabde170ebdb75e
SofiDevO <angiemeow89@gmail.com>
2023-10-15 04:52:35 -0600

    rutas relativas

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 		<div
 			class="flex justify-between mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8"
 		>
-			<img class="w-20" src="../../public/images/profile.png" alt="" />
+			<img class="w-20" src="../../images/profile.png" alt="" />
 
 			<ul class="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
 				<li>
