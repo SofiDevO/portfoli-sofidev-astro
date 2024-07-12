035a823e29b65d2ee1b362442dfb9f0f4a0e786f
SofiDev <angiemeow89@gmail.com>
2024-07-12 02:06:04 -0600

    ✨ Refactor styles
    Se separa la lógica en componentes más pequeños(atomic design). Se continua con la migración de estilos de tailwind a SASS

---
+++ src/controllers/getUserInfo.js
@@ -0,0 +1,10 @@
+// getUserInfo.js
+import userData from "@src/data/userData";
+
+// Función para obtener la información del usuario por índice
+const getUserInfo = userData.map((data) => {
+  return data;
+});
+
+
+export default getUserInfo[0];
---
+++ src/data/userData.js
@@ -0,0 +1,15 @@
+const userData = [
+    {
+        name: "Sofi",
+        lastName: "Dev",
+        profilePicture: "../images/profile-pic.png",
+        about:"Me llamo Ángela Sofía Osorio. Soy desarrolladora web con 1 año y medio de experiencia. <br><br> Puedes encontrarme en mis redes sociales  ",
+        sloganHero1: "A developer that Judges a book by its cover...",
+        sloganHero2: "Because if the cover does not impress you what else can?",
+    }
+
+]
+
+
+
+export default userData;
